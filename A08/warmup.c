f1acdee04da38ff49fe6ff7ec01391e9ff480a77
EmmaYusijia <syu4@haverford.edu>
2024-11-01 19:05:25 -0400

    Update warmup.c

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main() {
+  pid_t pid;
+  int status;
+  printf("%d] A0\n", getpid());
+  pid = fork();
+
+  if(pid == 0){
+    printf("%d] B1\n", getpid());
+  }else{
+    printf("%d] B0\n", getpid());
+    pid = fork();
+    if(pid == 0){
+      printf("%d] C1\n", getpid());
+    }else{
+      printf("%d] C0\n", getpid());
+    }
+  }
+  printf("%d] Bye\n", getpid());
+  exit(0);
   return 0;
 }
